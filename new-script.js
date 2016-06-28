17453fc9c6d6a50a8594085b9aed949d6de73da5
Dan Lynn <lynnd335@gmail.com>
2016-06-28 09:54:42 -0400

    clicks improved

--- new-script.js
+++ new-script.js
@@ -1,9 +1,9 @@
 $(function() {
     var charScore = [{ "char": "A", "value": 1 }, { "char": "B", "value": 3 }, { "char": "C", "value": 3 }, { "char": "D", "value": 2 }, { "char": "E", "value": 1 }, { "char": "F", "value": 4 }, { "char": "G", "value": 2 }, { "char": "H", "value": 4 }, { "char": "I", "value": 1 }, { "char": "J", "value": 8 }, { "char": "K", "value": 5 }, { "char": "L", "value": 1 }, { "char": "M", "value": 3 }, { "char": "N", "value": 1 }, { "char": "O", "value": 1 }, { "char": "P", "value": 3 }, { "char": "Q", "value": 1 }, { "char": "R", "value": 1 }, { "char": "S", "value": 1 }, { "char": "T", "value": 1 }, { "char": "U", "value": 1 }, { "char": "V", "value": 4 }, { "char": "W", "value": 4 }, { "char": "X", "value": 8 }, { "char": "Y", "value": 4 }, { "char": "Z", "value": 1 }];
-    var chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "E", "I", "O", "U", "Y", "R", "L", "S", "T", "N"];
 
     var box = [
-        ["", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "D", "O", "G", "", "", "", ""],
         ["", "", "", "", "", "", "", "", "", ""],
         ["", "", "", "", "", "", "", "", "", ""],
         ["", "", "", "", "", "", "", "", "", ""],
@@ -38,14 +38,14 @@ $(function() {
     }
     //
     function unique(array) {
-    return $.grep(array, function(el, index) {
-        return index == $.inArray(el, array);
-    });
-}
+        return $.grep(array, function(el, index) {
+            return index == $.inArray(el, array);
+        });
+    }
     //
     Array.prototype.unique = function() {
             var arr = this;
-            console.log(arr);
+            // console.log(arr);
             return $.grep(arr, function(v, i) {
                 return $.inArray(v, arr) === i;
             });
@@ -53,7 +53,9 @@ $(function() {
         //
     function randLetters(row) {
         for (var i = 0; i < row.length; i++) {
-            row[i] = chars[Math.floor((Math.random() * 25) + 0)];
+            if (row[i] === "") {
+                row[i] = chars[Math.floor((Math.random() * chars.length) + 0)];
+            }
         }
     }
     //
@@ -66,6 +68,7 @@ $(function() {
         }
     }
     //
+
     //
     function setGame() {
         for (var i = 0; i < box.length; i++) {
@@ -75,17 +78,14 @@ $(function() {
 
         for (var n = 0; n < box.length; n++) {
             for (var i = 0; i < box[n].length - 1; i++) {
-                box[n][i] = box[n][i] + "</div></td><td><div class='tile' id='" + n + "/" + i + "'>"
+                box[n][i] = box[n][i] + "</div></td><td id='td" + n + "/" + (i + 1) + "''><div class='tile' id='" + n + "/" + (i + 1) + "'>"
             }
-            box[n] = "</div></td></tr><tr><td><div class='tile' id='" + n + "/0''>" + box[n].join("");
+
+            box[n] = "</div></td></tr><tr><td id='td" + n + "/" + 0 + "''><div class='tile' id='" + n + "/" + 0 + "''>" + box[n].join("");
             document.getElementById("box").innerHTML = "<tbody><tr><td>" + box.join("") + "</tbody>";
         }
 
     }
-
-
-
-    
 
 
     //
@@ -103,6 +103,15 @@ $(function() {
             return true;
         } else {
             console.log("Not a word!");
+            $(".selected-tile").each(function(t) {
+                $(this).removeClass("selected-tile");
+                $(this).addClass("tile");
+                $(this).animate({
+                    backgroundColor: "rgba(252,255,0,0.7)",
+                    border: "2px solid black",
+                    color: "black"
+                }, 300);
+            });
             textSelected = [];
             selected = [];
             return false;
@@ -111,29 +120,54 @@ $(function() {
 
     //
     var collect = function() {
-        console.log(selected);
-        for (var i = 1; i < selected.length; i++){
-            var id = selected[i].attr('id');
-            console.log(id);
-            if(id === selected[i - 1].attr('id')){
-                selected.splice(selected[i - 1],1);
-                console.log(selected[i -1] + " spliced")
-            }
-        }
-        console.log(selected);
-        selected = removeBlanks(selected);
+        selected = removeBlanks(selected.unique());
+
+        var newSelected = [];
+
         console.log(selected);
         for (var i = 0; i < selected.length; i++) {
-            
-                textSelected.push(selected[i].text());
-                
-                console.log(selected[i].attr('id') + " / typeof = " + typeof selected[i]);
-                console.log(selected[i].text() + " / typeof = " + typeof selected[i].text());
+            if (selected[i].attr("id") !== undefined) {
+                if (selected[i].text() !== "") {
+                    newSelected.push({ "div": selected[i].attr("id"), "text": selected[i].text(), "index": i, "jq": selected[i] });
+                }
+            }
         }
-        text = removeBlanks(textSelected);
+
+        var newtext = [];
+
+        console.log(newSelected);
+        for (var i = 0; i < newSelected.length; i++) {
+            if (newSelected[i] === undefined) {
+                console.log("misfire @ " + newSelected[i]);
+            } else {
+                console.log("input correct @ " + newSelected[i]);
+                newtext.push(newSelected[i].text);
+            }
+        }
+        // console.log("newtext = " + newtext);
+        //
+        // arr = jQuery.map(arr, function(n, i) {
+        //     return (n.toUpperCase() + i);
+        // });
+
+
+        //
+
+
+        //
+        //
+        // var groups = _.groupBy(selected, 1);
+        // var result = _.map(divsSelected, function (i) { return groups[i].shift(); });
+
+        // for (var i = 0; i < result.length; i++){
+        //     console.log(result[i].text());
+        // }
+        // 
+        text = removeBlanks(newtext);
+
         selected = removeBlanks(selected.unique());
-        console.log(selected);
-        console.log(textSelected);
+        // console.log(selected);
+        console.log("text =" + text);
         return text;
     };
     //
@@ -144,29 +178,108 @@ $(function() {
                 border: "2px solid blue",
                 color: "white"
             }, 300);
+            tile.removeClass("tile");
             tile.addClass("selected-tile");
             selected.push(tile);
         }
         //
-    var popTile = function(cell){
-        var coord = cell.position().top + cell.height();
-        console.log(coord);
-        cell.animate({
-            width: "0px",
-            height: "0px",
-            borderWidth: "0px",
-            fontSize: "0px",
-            lineHeight: "0px"
-        }, 200 );
-        return coord; 
+    var popTile = function(cell) {
+        console.log("popTile cell id = " + cell.attr("id"));
+        $(".selected-tile").each(function(t) {
+            $(this).animate({
+                width: "0px",
+                height: "0px",
+                borderWidth: "0px",
+                fontSize: "0px",
+                lineHeight: "0px"
+            }, 200);
+        });
+        if (cell.attr("id") !== undefined) {
+            var coord = {
+                "bottom": cell.position().top + cell.height(),
+                "left": cell.position().left + cell.width(),
+                "row": parseInt(cell.attr("id").split("/")[0]),
+                "col": parseInt(cell.attr("id").split("/")[1]),
+                "char": cell.text(),
+                "tile": cell,
+                "td": cell.parent()
+            };
+            console.log(coord);
+
+            return coord;
+        } else {
+            return 0;
+        }
+
     };
-    var dropTiles = function(cell, coord, movement){
-        var oldFloor = cell.attr("bottom").parseInt(); 
-        cell.animate({
-            bottom: "20px"
-        }, 200 * movement)
-    }    
-        //
+
+
+    function newRando(td, row, col) {
+        var letter = chars[Math.floor((Math.random() * chars.length) + 0)];
+        var cell = $(document.getElementById("td" + row + "/" + col));
+        cell.html("<div class='new-tile'></div>");
+        var newtile = td.children(".new-tile");
+        console.log(newtile.text());
+        newtile.animate({
+            width: "100%",
+            height: "100%",
+            fontSize: "19px",
+            lineHeight: "45px",
+            color: "black",
+            border: "2px solid #000",
+            borderRadius: "10%"
+        }, 300);
+        newtile.animate({
+            backgroundColor: "rgba(252,255,0,0.7)"
+        }, 450);
+        setTimeout(function() {
+           td.html("<div class='tile' id='" + row + "/" + col + "''>" + letter + "</div>"); 
+        }, 500)
+
+    }
+
+    var topRowTiles = function(coord){
+        var cell = $(document.getElementById(coord.td));
+        
+    }
+    
+
+    var dropTiles = function(coord) {
+        var slid = false;
+        var upper = document.getElementById((coord.row - 1) + "/" + coord.col);
+        var upCell = $(document.getElementById((coord.row - 1) + "/" + coord.col));
+        var upTD = $(upper).parent();
+        console.log("upcell text = " + upCell.text());
+        var downSlide = (coord.bottom - (upCell.position().top)) + 14 + "px";
+        // var downSlide = "47px";
+        console.log(downSlide);
+
+        // console.log(upTD.attr("id"));
+        var char = upCell.text();
+        // newRando(upTD, (coord.row - 1), coord.col);
+        
+        $(upper).animate({
+                top: downSlide
+            }, 300);
+
+        setTimeout(function() {
+            console.log("timer running");
+            console.log("slid = true")
+            var currentTD = $(document.getElementById("td" + (coord.row) + "/" + coord.col));
+            currentTD.html("<div class='tile' id='" + (coord.row) + "/" + coord.col + "'>" + char + "</div");
+            newRando(upTD, (coord.row - 1), coord.col);
+            slid = false;
+        }, 400);
+        // console.log(slid);
+        // if (slid === true) {
+                // console.log("slid = true")
+                // var currentTD = $(document.getElementById("td" + (coord.row) + "/" + coord.col));
+                // currentTD.html("<div class='tile' id='" + (coord.row) + "/" + coord.col + "'>" + char + "</div");
+                // newRando(upTD, (coord.row - 1), coord.col);
+                // slid = false;
+        // }
+    }
+
 
     setGame();
 
@@ -179,25 +292,42 @@ $(function() {
             isDown = false;
             var word = collect().join("");
             var scoreWord = checkWord(word);
-            if (scoreWord === true){
+            if (scoreWord === true) {
                 var tds = [];
                 for (var i = 0; i < selected.length; i++) {
                     tds.push(popTile(selected[i]));
                 }
+                for (var i = 0; i < tds.length; i++) {
+                    if (tds[i].row > 0) {
+                        dropTiles(tds[i]);
+                    }else if(tds[i].row === 0){
+                        newRando(tds[i].td, tds[i].row, tds[i].col);
+                    }
+
+                }
+
+                // setTimeout(function() {
+                //     $(".selected-tile").each(function(t) {
+                //         $(this).parent("td").html(
+                //             "<div class='tile' id='" + $(this).children(".tile").attr("id") + "'>" +  "</div>"
+                //             )
+                //     });
+                // }, 500)
                 selected = [];
                 textSelected = [];
 
-            }else{
+
+
+            } else {
                 $("#box").effect("shake", 15);
-                $(".selected-tile").animate({
-                    backgroundColor: "rgba(252,255,0,0.7)",
-                    border: "2px solid black",
-                    color: "black"
-                }, 300)
-                .addClass("tile");
+                $(".selected-tile").each(function(t) {
+                    $(this).removeClass("selected-tile")
+                    $(this).addClass("tile")
+                });
                 selected = [];
                 textSelected = [];
             }
+
 
         })
     $("td").click(function() {
@@ -207,7 +337,7 @@ $(function() {
             var tile = $(this).children(".tile");
         }
 
-    });    
+    });
 
 
     $("td").mouseover(function() {
@@ -218,9 +348,7 @@ $(function() {
         }
 
     });
-// B / typeof = string
-// new-script.js:113 undefined / typeof = object
-// new-script.js:114  / typeof = string
+
 
 
 });
